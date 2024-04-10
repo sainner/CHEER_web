a95d6af2ab5eb9fd65ba98635f6bd659d46aedaa
Sainner <sainner@qq.com>
2024-04-11 00:17:31 +0800

    config

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+//database configuration
+define('DB_HOST', 'localhost'); // 数据库服务器地址
+define('DB_USERNAME', 'your_username'); // 数据库用户名
+define('DB_PASSWORD', 'your_password'); // 数据库密码
+define('DB_NAME', 'your_database_name'); // 数据库名
+
+
+// Base URL
+define('BASE_URL', 'http://localhost:3000/');
